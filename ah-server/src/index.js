2a19abf49ad4aaafb9c84ce43b089ee348b7d6d0
Rene Cardozo <rene.cardozo@truextend.com>
2017-05-18 09:59:27 -0400

    Fixed code review obsevations

--- ah-server/src/index.js
+++ ah-server/src/index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import Config from './config/config'
 import Db from './database/db-manager';
+import routes from './routes/accouting-home.route';
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-var routes = require('./routes/accouting-home.route');
+
 routes(app);
 
 app.listen(Config.port, () => {
