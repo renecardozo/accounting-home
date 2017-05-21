1ee7a33a551b40961e97fa7a1981fa5874175c5b
eden ariel guzman andia <edenarielguzmanandia@MacBook-Pro-de-eden.local>
2017-05-21 08:14:57 -0400

    AH-32: add bootstrap library

--- ah-client/src/app/app.module.ts
+++ ah-client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TableComponentComponent } from './table-component/table-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponentComponent
   ],
   imports: [
     BrowserModule,
