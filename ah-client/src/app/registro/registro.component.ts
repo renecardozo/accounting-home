ae121df89e59b4e2d83b7b46366e62f43e242c7a
rodri <jimibilibob@hotmail.com>
2017-06-06 21:38:46 -0400

    IU registro de usuario

---
+++ ah-client/src/app/registro/registro.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'registro-component',
+  templateUrl: 'registro.component.html',
+  styleUrls: ['registro.component.css']
+})
+export class RegistroComponent{}
